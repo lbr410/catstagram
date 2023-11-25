6e8ddee58676922cb863e7b3aa2c5d20e2c68f54
lbr <lbor3280@naver.com>
2023-11-25 19:29:37 +0900

    팔로우 추천 기능 완료

--- src/main/java/com/catstagram/follow/model/FollowDTO.java
+++ src/main/java/com/catstagram/follow/model/FollowDTO.java
@@ -15,4 +15,10 @@ public class FollowDTO {
 	private int follow_idx;
 	private int member_from;
 	private int member_to;
+	private int following_idx_of_my_following; // 내가 친구 추가한 사람의 친구의 idx
+	private String following_id_of_my_following; // 내가 친구 추가한 사람의 친구의 id
+	private String following_name_of_my_following; // 내가 친구 추가한 사람의 친구의 name
+	private String following_img_of_my_following; // 내가 친구 추가한 사람의 친구의 img
+	private int num_of_followers; // 내가 친구 추가한 사람들 중에서 이 사람을 몇 명이나 친구 추가했는지(ex. '팔로잉 외 1명'이므로 -1이 적용됨)
+	private String my_following_list; // 이 사람을 내가 친구 추가한 사람들 중에 누가누가 친구 추가했는지
 }
--- src/main/java/com/catstagram/mapper/FollowMapper.java
+++ src/main/java/com/catstagram/mapper/FollowMapper.java
@@ -8,4 +8,6 @@ public interface FollowMapper {
 	public int following(FollowDTO dto);
 	public int cancelFollowing(FollowDTO dto);
 	public int delFollower(Map map);
+	public FollowDTO suggestedFollowers(int sidx);
+	public FollowDTO suggestedFollowersInMain(int sidx);
 }
