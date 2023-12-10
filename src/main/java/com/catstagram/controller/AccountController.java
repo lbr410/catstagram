c61fed8b079be74d1576232d508332812b5e3aab
lbr <lbor3280@naver.com>
2023-12-11 07:57:38 +0900

    url 수정

--- src/main/java/com/catstagram/controller/AccountController.java
+++ src/main/java/com/catstagram/controller/AccountController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccountController {
 	
-	@GetMapping("/catstagram/account/{url}")
+	@GetMapping({"/catstagram/account/{url}", "/catstagram/{url}/{url2}"})
 	public String accountUrl() {
 		return "emptyPage";
 	}
