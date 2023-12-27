9a5f798136ff95965a1dcb34b26a24d1eb40cf5a
BB\lbr41 <lbor3280@naver.com>
2023-12-27 23:55:04 +0900

    setting

--- src/main/java/com/catstagram/CatstagramApplication.java
+++ src/main/java/com/catstagram/CatstagramApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @MapperScan(basePackages = "com.catstagram.mapper")
 @SpringBootApplication
 public class CatstagramApplication extends SpringBootServletInitializer {
+	
+//	public CatstagramApplication() {
+//		
+//	}
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
